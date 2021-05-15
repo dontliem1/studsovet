28dc9f26a894a092b444f135c037c2f2c5da7fd7
dontliem1 <dontliem1@gmail.com>
2021-05-15 16:08:57 +0500

    Удалил лишние файлы и добавил необходимые картинки

--- .eleventy.js
+++ .eleventy.js
@@ -1,5 +1,4 @@
 const yaml = require("js-yaml");
-const { DateTime } = require("luxon");
 const syntaxHighlight = require("@11ty/eleventy-plugin-syntaxhighlight");
 const htmlmin = require("html-minifier");
 
@@ -9,13 +8,6 @@ module.exports = function (eleventyConfig) {
 
   // Merge data instead of overriding
   eleventyConfig.setDataDeepMerge(true);
-
-  // human readable date
-  eleventyConfig.addFilter("readableDate", (dateObj) => {
-    return DateTime.fromJSDate(dateObj, { zone: "Asia/Yekaterinburg" }).toFormat(
-      "dd LLL yyyy"
-    );
-  });
 
   // Syntax Highlighting for Code blocks
   eleventyConfig.addPlugin(syntaxHighlight);
@@ -33,6 +25,7 @@ module.exports = function (eleventyConfig) {
 
   // Copy Image Folder to /_site
   eleventyConfig.addPassthroughCopy("./src/static/img");
+  eleventyConfig.addPassthroughCopy("./src/static/font");
 
   // Copy favicon to route of /_site
   eleventyConfig.addPassthroughCopy("./src/favicon.ico");
